2c93709a2c8d502714d561fc05b90bbc43a54a32
Pravi-n <poshmanipravin1@gmail.com>
2021-04-21 12:25:59 +0530

    routes added

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -12,5 +12,23 @@ export default [
     title: 'Second Page',
     icon: <Mail size={20} />,
     navLink: '/second-page'
+  },
+  {
+    id: 'CurrencyList',
+    title: 'Currrency List Page',
+    icon: <Mail size={20} />,
+    navLink: '/currency-List'
+  },
+  {
+    id: 'CityList',
+    title: 'City List Page',
+    icon: <Mail size={20} />,
+    navLink: '/city-List'
+  },
+  {
+    id: 'Category',
+    title: 'Category List Page',
+    icon: <Mail size={20} />,
+    navLink: '/category-List'
   }
 ]
