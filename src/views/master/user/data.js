659b1830d255cdf58091fb45dddf6720970df7ba
ttejas123 <tthakare73@gmail.com>
2021-04-28 15:36:32 +0530

    demo

---
+++ src/views/master/user/data.js
@@ -0,0 +1,186 @@
+// Bid id
+// User
+//tatus - created,Japan,USA,closed,rejected,India
+// Requested Customization
+// Customization Status : pending, approval, approved, 
+// catalogue id
+// Req Quantity
+// created time
+// Go Japan date
+// Bid Close Date
+// Bid Application Date
+// Nos of bidder
+// DeJapanry date
+// Buyer Budget
+// DeJapanry Location : Seller Or Buyer location
+// finalize seller id
+import komal  from '../../../assets/images/logo/komal.jpg'
+import pravin  from '../../../assets/images/logo/pravin.jpg'
+import himanshu  from '../../../assets/images/logo/himanshu.jpg'
+import reethika  from '../../../assets/images/logo/reethika.jpg'
+
+
+//expot data
+export const data = [
+      {
+        CreatedDate :"30-10-2021",
+        email: "tthakare73@gmail.com",
+        
+        mobileNo : 321342,
+        
+        userType: [{value: "Seller", label: "Seller"}],  //Buyer Seller
+       
+        isVerified : [{value: "approval", label: "approval"}],
+        profileScore: 1,
+
+        created : "04-10-2021",
+        country : [{value: "USA", label: "USA"}],
+        state : [{value: "Jorgi", label: "Jorgi"}],
+        city : [{value: "New York", label: "New York"}],
+        lastName: "Thakare",
+        firstName: "Tejas"
+      },
+      {
+        CreatedDate :"23-10-2021",
+        email: "pravinP@gmail.com",
+        
+        mobileNo : 321342,
+        
+        userType: [{value: "Buyer", label: "Buyer"}],  //Buyer Seller
+        
+        isVerified : [{value: "pending", label: "pending"}],
+        profileScore: 2,
+        ReqCustomization :"differnt Color",
+        created : "07-10-2021",
+        country : [{value: "Japan", label: "Japan"}],
+        state : [{value: "Osaka", label: "Osaka"}],
+        city : [{value: "kimoye", label: "kimoye"}],
+        lastName: "Poshmani",
+        firstName: "Pravin"
+      },
+      {
+        CreatedDate :"13-10-2021",
+        email: "komal@gmail.com",
+        mobileNo : 541342,
+        
+        userType: [{value: "Both", label: "Both"}], //Buyer Seller
+       
+        isVerified : [{value: "approved", label: "approved"}],
+        profileScore: 1,
+        ReqCustomization :"Company WaterMark",
+        created : "04-10-2021",
+        country : [{value: "India", label: "India"}],
+        state : [{value: "kerla", label: "kerla"}],
+        city : [{value: "sharana", label: "sharana"}],
+        lastName: "Kamble",
+        firstName: "komal"
+      },
+      {
+        CreatedDate :"21-10-2021",
+        email: "mehul@gmail.com",
+        
+        mobileNo : 721342,
+        
+        userType: [{value: "Seller", label: "Seller"}],  //Buyer Seller
+        
+        isVerified : [{value: "approval", label: "approval"}],
+        profileScore: 3,
+
+        created : "11-10-2021",
+        country : [{value: "Viatnam", label: "Viatnam"}],
+        state : [{value: "nakoi", label: "nakoi"}],
+        city : [{value: "noki", label: "noki"}],
+        lastName: "B",
+        firstName: "Mehul"
+      },
+      {
+        CreatedDate :"30-10-2021",
+        email: "harpriya@gmail.com",
+        
+        mobileNo : 2331342,
+        
+        userType: [{value: "Both", label: "Both"}],  //Buyer Seller
+        
+        isVerified : [{value: "pending", label: "pending"}],
+        profileScore: 1,
+
+        created : "31-10-2021",
+        country : [{value: "Sweedan", label: "Sweedan"}],
+        state : [{value: "seedon", label: "seedon"}],
+        city : [{value: "nipu", label: "nipu"}],
+        lastName: "H",
+        firstName: "Harpriya"
+      },
+      {
+        CreatedDate :"22-10-2021",
+        email: "reethika@gmail.com",
+        
+        mobileNo : 771356,
+        
+        userType: [{value: "Buyer", label: "Buyer"}],  //Buyer Seller
+        
+        isVerified : [{value: "approved", label: "approved"}],
+       profileScore: 4,
+
+        created : "30-10-2021",
+        country : [{value: "India", label: "India"}],
+        state : [{value: "UtterPradesh", label: "UtterPradesh"}],
+        city : [{value: "lihipur", label: "lihipur"}],
+        lastName: "R",
+        firstName: "Reethika"
+      },
+      {
+        CreatedDate :"29-10-2021",
+        email: "himanshu@gmail.com",
+        
+        mobileNo : 671356,
+        
+        userType: [{value: "Seller", label: "Seller"}],  //Buyer Seller
+        
+        isVerified : [{value: "pending", label: "pending"}],
+        profileScore: 1,
+
+        created : "12-10-2021",
+        country : [{value: "Japan", label: "Japan"}],
+        state : [{value: "Osaka", label: "Osaka"}],
+        city : [{value: "lipi", label: "lipi"}],
+        lastName: "H",
+        firstName: "Himanshu"
+      },
+      {
+        CreatedDate :"13-10-2021",
+        email: "suryaDas@gmail.com",
+        
+        mobileNo : 921345,
+        
+        userType: [{value: "Seller", label: "Seller"}],  //Buyer Seller
+        
+        isVerified : [{value: "approved", label: "approved"}],
+        profileScore: 5,
+
+        created : "22-10-2021",
+        country : [{value: "India", label: "India"}],
+        state : [{value: "Maharashtra", label: "Maharashtra"}],
+        city : [{value: "Navi Mumbai", label: "Navi Mumbai"}],
+        lastName: "Das",
+        firstName: "Surya"
+      },
+      {
+        CreatedDate :"03-10-2021",
+        email: "avinash@gmail.com",
+        
+        mobileNo : 3421332,
+       
+        userType: [{value: "Buyer", label: "Buyer"}],  //Buyer Seller
+        
+        isVerified : [{value: "approval", label: "approval"}],
+        profileScore: 1,
+
+        created : "04-10-2021",
+        country : [{value: "USA", label: "USA"}],
+        state : [{value: "pencilVenia", label: "pencilVenia"}],
+        city : [{value: "created", label: "created"}],
+        lastName: "A",
+        firstName: "Avinash"
+      }
+]
---
+++ src/views/master/user/formModel.js
@@ -0,0 +1,389 @@
+// ** Custom Components
+import Avatar from '@components/avatar'
+import Select from 'react-select'
+// ** Third Party Components
+import axios from 'axios'
+import { MoreVertical, User, Users, Edit, Calendar, FileText, Archive, Trash,  MapPin, DollarSign, X  } from 'react-feather'
+//import Flatpickr from 'react-flatpickr'
+// ** React Imports
+import { useState, useEffect } from 'react'
+import { selectThemeColors, isObjEmpty } from '@utils'
+// ** Third Party Components
+import Flatpickr from 'react-flatpickr'
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, InputGroup, InputGroupAddon, InputGroupText, Input, Label, Badge, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem  } from 'reactstrap'
+
+// ** Styles
+import '@styles/react/libs/flatpickr/flatpickr.scss'
+
+const AddNewModal = (prop) => {
+  const optionDLocation = [
+    {value: "Multiple", label: "Multiple"},
+    {value: "Single", label: "Single"}
+  ]
+  const optionCustomStatus = [
+    {value: "pending", label: "pending"},
+    {value: "approval", label: "approval"},
+    {value: "approved", label: "approved"}
+  ]
+  
+  const optionBidStatus = [
+    {value: "created", label: "created"},
+    {value: "live", label: "live"},
+    {value: "extended", label: "extended"},
+    {value: "closed", label: "closed"},
+    {value: "rejected", label: "rejected"},
+    {value: "auto closed", label: "auto closed"}
+  ]
+
+  const initialvalues = {
+    Name: "",
+    DeliveryDate: new Date(),
+    BidCloseDate: new Date(),
+    NoOfBidder: 0,
+    BidApplicationDate: new Date(),
+    created: new Date(),
+    GoLive: new Date(),
+    DeliveryLocation : "",
+    ReqQuantity: 0,
+    BuyerBudget: 0,
+    CustomStatus: "",
+    ReqCustomization: "",
+    BidStatus: ""
+  }
+  const [selectedOption, setselectedOption] = useState()
+  const [values, setValues] = useState(initialvalues)
+  const [allDay, setAllDay] = useState(false)
+  const [endPicker, setEndPicker] = useState(new Date())  
+  // ** Custom close btn
+  const CloseBtn = <X className='cursor-pointer' size={15} onClick={prop.handleModal} />
+
+  useEffect(() => {
+    if (prop.currentId === "") {
+        setValues({...initialvalues})
+    } else {
+        setValues({
+          ...prop.data[prop.currentId - 1]
+        })
+    }
+  }, [prop.currentId, prop.data])
+  //for other input
+  const handleInputeChange = (event) => {
+    const {name, value} = event.target
+    setValues(
+    {
+      ...values,
+      [name] : value
+    }
+    )
+  }
+  //for drop down
+  const handleChange = (selectedOption) => {
+    
+    setValues(
+      {
+        ...values,
+        Currency : selectedOption.value
+      }
+    )
+  }
+
+  const submitHandle = (event) => {
+    console.log(values)
+    prop.editAction(values)
+    setValues(initialvalues)
+  }
+
+  return (
+    <Modal
+      isOpen={prop.open}
+      toggle={prop.handleModal}
+      className='sidebar-sm'
+      modalClassName='modal-slide-in'
+      contentClassName='pt-0'
+    >
+      <ModalHeader className='mb-3' toggle={prop.handleModal} close={CloseBtn} tag='div'>
+        <h5 className='modal-title'>Edit Record</h5>
+      </ModalHeader>
+      <ModalBody className='flex-grow-1'>
+       
+        <FormGroup>
+          <Label for='Name'>User Name</Label>
+          <InputGroup>
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText>
+                <User size={15} />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Input name="Name" disabled="disabled" onChange={handleInputeChange} id='Name' placeholder='Afghanistan' value={values.Name} />
+          </InputGroup>
+        </FormGroup>
+
+        <FormGroup>
+            <Label for='created'>Bid Created Date</Label>
+             <InputGroup>
+              <InputGroupAddon addonType='prepend'>
+                <InputGroupText>
+                  <Calendar size={15} />
+                </InputGroupText>
+              </InputGroupAddon>
+              <Flatpickr
+              disabled="disabled"
+              required
+              id='endDate'
+              // tag={Flatpickr}
+              name='created'
+              className='form-control'
+              onChange={(date) => setValues(
+                                          {
+                                            ...values,
+                                            created : date[0]
+                                          }
+                        )}
+              value={values.created}
+              options={{
+                dateFormat: 'd-m-Y'
+              }}
+            />
+          </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='BidApplicationDate'>Bid Application Date</Label>
+           <InputGroup>
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText>
+                <Calendar size={15} />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Flatpickr
+              disabled="disabled"
+              required
+              id='endDate'
+              // tag={Flatpickr}
+              name='BidApplicationDate'
+              className='form-control'
+              onChange={(date) => setValues(
+                                          {
+                                            ...values,
+                                            BidApplicationDate : date[0]
+                                          }
+                        )}
+              value={values.BidApplicationDate}
+              options={{
+                dateFormat: 'd-m-Y'
+              }}
+            />
+          </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='GoLive'>Go Live Date</Label>
+           <InputGroup>
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText>
+                <Calendar size={15} />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Flatpickr
+              required
+              id='endDate'
+              // tag={Flatpickr}
+              name='GoLive'
+              className='form-control'
+              onChange={(date) => setValues(
+                                        {
+                                             ...values,
+                                            GoLive : date[0]
+                                        }
+                                   )}
+              value={values.GoLive}
+              options={{
+                dateFormat: 'd-m-Y'
+              }}
+            />
+          </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='DeliveryLocation'>Delivery Location</Label>
+            <Select
+              id='DeliveryLocation'
+              className='react-select'
+              classNamePrefix='select'
+              isClearable={false}
+              options={optionDLocation}
+              theme={selectThemeColors}
+              value={values.DeliveryLocation[0]}
+              onChange={data => {
+                                 setValues(
+                                          {
+                                             ...values,
+                                             DeliveryLocation : data
+                                          } 
+                                  )
+                                }
+                        }
+            />
+          </FormGroup>
+
+        <FormGroup>
+            <Label for='DeliveryDate'>Delivery Date</Label>
+            <InputGroup>
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText>
+                <Calendar size={15} />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Flatpickr
+              required
+              id='endDate'
+              // tag={Flatpickr}
+              name='DeliveryDate'
+              className='form-control'
+              onChange={(date) => setValues(
+                                                                {
+                                                                  ...values,
+                                                                  DeliveryDate : date[0]
+                                                                }
+                                                        )}
+              value={values.DeliveryDate}
+              options={{
+                dateFormat: 'd-m-Y'
+              }}
+            />
+          </InputGroup>
+          </FormGroup>
+
+        <FormGroup>
+            <Label for='BidCloseDate'>Bid Close Date</Label>
+            <InputGroup>
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText>
+                <Calendar size={15} />
+              </InputGroupText>
+            </InputGroupAddon>
+            <Flatpickr
+              disabled="disabled"
+              required
+              id='endDate'
+              // tag={Flatpickr}
+              name='BidCloseDate'
+              className='form-control'
+              onChange={(date) => setValues(
+                                                                {
+                                                                  ...values,
+                                                                  BidCloseDate : date[0]
+                                                                }
+                                                        )}
+              value={values.BidCloseDate}
+              options={{
+                dateFormat: 'd-m-Y'
+              }}
+            />
+          </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='Bidder'>Number Of Bidders</Label>
+            <InputGroup>
+              <InputGroupAddon addonType='prepend'>
+                <InputGroupText>
+                  <Users size={15} />
+                </InputGroupText>
+              </InputGroupAddon>
+              <Input type="number" disabled="disabled" name="NoOfBidder" onChange={handleInputeChange} id='NoOfBidder' placeholder='200' value={values.NoOfBidder} />
+            </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='BuyerBudget'>Buyer Budgets</Label>
+            <InputGroup>
+              <InputGroupAddon addonType='prepend'>
+                <InputGroupText>
+                  <Users size={15} />
+                </InputGroupText>
+              </InputGroupAddon>
+              <Input type="number" disabled="disabled" name="BuyerBudget" onChange={handleInputeChange} id='BuyerBudget' placeholder='200' value={values.BuyerBudget} />
+            </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='ReqQuantity'>Required Quantity</Label>
+            <InputGroup>
+              <InputGroupAddon addonType='prepend'>
+                <InputGroupText>
+                  <Users size={15} />
+                </InputGroupText>
+              </InputGroupAddon>
+              <Input type="number" name="ReqQuantity" onChange={handleInputeChange} id='ReqQuantity' placeholder='200' value={values.ReqQuantity} />
+            </InputGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='BidStatus'>Bid Status</Label>
+            <Select
+              id='BidStatus'
+              className='react-select'
+              classNamePrefix='select'
+              isClearable={false}
+              options={optionBidStatus}
+              theme={selectThemeColors}
+              value={values.BidStatus[0]}
+              onChange={data => {
+                                 setValues(
+                                          {
+                                             ...values,
+                                             BidStatus : data
+                                          } 
+                                  )
+                                }
+                        }
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='CustomStatus'>Customization Status</Label>
+            <Select
+              id='CustomStatus'
+              className='react-select'
+              classNamePrefix='select'
+              isClearable={false}
+              options={optionCustomStatus}
+              theme={selectThemeColors}
+              value={values.CustomStatus[0]}
+              onChange={data => {
+                                 setValues(
+                                          {
+                                             ...values,
+                                             CustomStatus : data
+                                          } 
+                                  )
+                                }
+                        }
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for='ReqCustomization'>Requested Customization</Label>
+            <InputGroup>
+              <Input type="text" name="ReqCustomization" onChange={handleInputeChange} id='ReqCustomization' placeholder='Company WaterMark' value={values.ReqCustomization} />
+            </InputGroup>
+          </FormGroup>
+
+        <Button className='mr-1' color='primary' onClick={ e =>  {
+                                                          prop.handleModal() 
+                                                          submitHandle()
+                                                        }
+                                                      }>
+          Submit
+        </Button>
+        <Button color='secondary' onClick={ prop.handleModal } outline>
+          Cancel
+        </Button>
+
+      </ModalBody>
+    </Modal>
+  )
+}
+
+export default AddNewModal
