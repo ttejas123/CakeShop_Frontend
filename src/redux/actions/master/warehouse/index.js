0d67079a195ac89b675050142696f18c82f8dfd5
TejasThakare <tthakare73@gmail.com>
2021-09-22 18:49:16 +0530

    ui

---
+++ src/redux/actions/master/warehouse/index.js
@@ -0,0 +1,183 @@
+import axios from 'axios'
+import { BaseUrl } from '@store/baseUrl' //Base Url
+//const link = `${BaseUrl}/countries` //Link For Country Crud
+
+//const BaseUrl = `http://159.65.156.12:1337/graphql`
+
+export const userAddressDropd = (limit, start) => {
+    // const UserData = JSON.parse(localStorage.getItem('userData'))
+    // const UserName = UserData === null ? ("") : (UserData.username)
+    const query = `query{
+  userAddresses{
+        id
+      address_name
+      area
+      street_name
+      pincode
+      city{
+        name
+        state{
+          name
+          country{
+            country_name
+          }
+        }
+      }
+  }
+}`
+
+    return dispatch => {
+        //List
+        axios.post(BaseUrl, {query}).then(res => {
+            //console.log(res.data.data)
+            return dispatch({
+                type: 'USERADDRESSDROPDOWN',
+                payload: res.data.data
+            })
+        })
+    }
+}
+
+export const List = (limit, start, search) => {
+    const UserData = JSON.parse(localStorage.getItem('userData'))
+    const UserName = UserData === null ? ("") : (UserData.username)
+    const searchPro = search ? search : ""
+    const query = `query{
+  userWarehousesConnection(limit: ${limit}, start: ${start}, where:{
+        name_contains: "${searchPro}"
+  }){
+    values{
+          id
+          name
+          address_1
+          createdAt
+          city{
+            name
+            state{
+              name
+              country{
+                country_name
+              }
+            }
+          }
+          pincode
+          is_bidoya_warehouse
+          landmark
+    }
+    aggregate{
+      count
+    }
+  }
+}`
+
+    return dispatch => {
+        //List
+        axios.post(BaseUrl, {query}).then(res => {
+            //console.log(res.data.data)
+            return dispatch({
+                type: 'WAREHOUSELIST',
+                payload: res.data.data
+            })
+        })
+    }
+}
+
+
+export const CreateWarehouse = (data) => {
+  console.log(data)
+    const createQ = `mutation{
+  createUserWarehouse(input:{data: {
+    name:"${data.warehouse}"
+    address_1: "${data.address1}"
+    city: "${data.City}"
+    landmark: "${data.address1}"
+    is_bidoya_warehouse: ${data.is_bidoya_wareHouse}
+    pincode: ${data.pincode}
+  }}){
+    userWarehouse{
+      name
+          address_1
+          createdAt
+          city{
+            name
+            state{
+              name
+              country{
+                country_name
+              }
+            }
+          }
+          pincode
+          is_bidoya_warehouse
+          landmark
+    }
+  }
+}`
+    //Create
+  return dispatch => {
+        //List
+        axios.post(BaseUrl, {query: createQ}).then(res => {
+            console.log(res.data.data)
+            return dispatch({
+                type: 'CREATEWAREHOUSE',
+                payload: res.data.data
+            })
+        })
+  }
+}
+
+export const EditC = (data) => {
+    //console.log(data)
+    const updateQ = `mutation{
+  updateUserAddress(input:{where: {id: "${data.id}"}, data:{address_name: "${data.area}", street_name: "${data.street_name}", area: "${data.area}", pincode: "${data.pincode}"}}){
+    userAddress{
+      id
+      address_name
+      area
+      street_name
+      pincode
+      city{
+        name
+        state{
+          name
+          country{
+            country_name
+          }
+        }
+      }
+      
+    }
+  }
+}`
+    
+    return dispatch => {
+        //List
+        axios.post(BaseUrl, {query: updateQ}).then(res => {
+            //console.log(res.data.data)
+            return dispatch({
+                type: 'USERADDRESSEDIT',
+                payload: res.data.data
+            })
+        })
+    }
+}
+
+export const DeleteWarehouse = (id) => {
+    const deleteQ = `mutation{
+  deleteUserWarehouse(input:{where: {id: "${id}"}}){
+    userWarehouse{
+      id
+    }
+  }
+}`
+    return dispatch => {
+        //List
+        axios.post(BaseUrl, {query: deleteQ}).then(res => {
+            //console.log(res.data.data)
+            return dispatch({
+                type: 'WAREHOUSEDELETE',
+                payload: res.data.data
+            })
+        })
+    }
+}
---
+++ src/redux/reducers/master/warehouse/index.js
@@ -0,0 +1,144 @@
+const initData = {
+     data: [],
+     count: 0,
+     dropdown: []
+}
+
+const todoReducer = (state = initData, action) => {
+
+    switch (action.type) {
+        case "WAREHOUSELIST":
+            //try {
+                const DataUserAddress = action.payload.userWarehousesConnection.values.map((val) => {  
+                    const CityName = val.city === null ? "" : val.city.name
+                    const StateName = val.city === null ? "" : val.city.state.name
+                    const CountryName = val.city === null ? "" : val.city.state.country.country_name
+                    return {
+                        address_1: val.address_1, 
+                        createdAt: val.createdAt,
+                        warehouse_name: val.name, 
+                        pincode: val.pincode, 
+                        city: CityName, 
+                        state: StateName,
+                        country: CountryName,
+                        landmark: val.landmark,
+                        is_bidoya_warehouse: val.is_bidoya_warehouse,  
+                        id: val.id
+                    }
+                })
+                //console.log(DataUserKyc)
+                return {
+                     ...state,
+                     count: action.payload.userWarehousesConnection.aggregate.count,
+                     data: DataUserAddress
+                }
+            // } catch (e) {
+            //     return {
+            //         ...state
+            //     }
+            // }
+        case "CREATEWAREHOUSE":
+            const UserAddressAdd = action.payload.createUserWarehouse.userWarehouse
+            if (UserAddressAdd !== null) {
+                const CityName = UserAddressAdd.city === null ? "" : UserAddressAdd.city.name
+                const StateName = UserAddressAdd.city === null ? "" : UserAddressAdd.city.state.name
+                const CountryName = UserAddressAdd.city === null ? "" : UserAddressAdd.city.state.country.country_name
+                
+                const AddData = {
+                        address_1: UserAddressAdd.address_1, 
+                        createdAt: UserAddressAdd.createdAt,
+                        warehouse_name: UserAddressAdd.name, 
+                        pincode: UserAddressAdd.pincode, 
+                        city: CityName, 
+                        state: StateName,
+                        country: CountryName,
+                        landmark: UserAddressAdd.landmark,
+                        is_bidoya_warehouse: UserAddressAdd.is_bidoya_warehouse,  
+                        id: UserAddressAdd.id
+                }
+
+                return {
+                    ...state,
+                    count: state.count + 1,
+                    data: [...state.data, AddData]
+                }
+            } else {
+                return {
+                    ...state
+                }
+            }
+            
+        case "USERADDRESSEDIT":
+        //console.log(action.payload)
+        const value = action.payload.updateUserAddress.userAddress 
+        //updateUserAddress name is already changed find somthing else
+
+            const filteredData = state.data
+                    .filter(
+                      stateId => (stateId.id !== value.id))
+            const CityName = value.city === null ? "" : value.city.name
+            const StateName = value.city === null ? "" : value.city.state.name
+            const CountryName = value.city === null ? "" : value.city.state.country.country_name
+                
+            const UpdateCountry = {
+                    area: value.address_name, 
+                    address_name: value.address_name, 
+                    pincode: value.pincode, 
+                    city: CityName, 
+                    state: StateName,
+                    street_name: value.street_name, 
+                    country: CountryName, 
+                    id: value.id
+                }
+            //console.log(filteredData)
+            return {
+                ...state,
+                data: [...filteredData, UpdateCountry]
+            }
+        case "WAREHOUSEDELETE":
+            const deleteUser = action.payload.deleteUserWarehouse.userWarehouse
+            if (deleteUser !== null) {
+                const DeleteData = state.data
+                        .filter(
+                          stateId =>
+                            /* eslint-disable operator-linebreak, implicit-arrow-linebreak */
+                            (stateId.id !== deleteUser.id)
+                        )
+                //console.log(DeleteData)
+                return {
+                    ...state,
+                    count: state.count - 1,
+                    data: DeleteData
+                }
+            } else {
+                return {
+                    ...state
+                }
+            }
+        case "USERADDRESSDROPDOWN":
+            try {
+                const DataDropDown = action.payload.userAddresses.map((val) => {  
+                    const CityName = val.city === null ? "" : val.city.name
+                    const StateName = val.city === null ? "" : val.city.state.name
+                    const CountryName = val.city === null ? "" : val.city.state.country.country_name
+                    return {
+                        label: `${val.address_name}, ${val.street_name}, ${CountryName}, ${StateName}, ${CityName}, ${val.pincode}`,
+                        value: `${val.address_name}, ${val.street_name}, ${CountryName}, ${StateName}, ${CityName}, ${val.pincode}`,  
+                        id: val.id
+                    }
+                })
+                //console.log(DataUserKyc)
+                return {
+                     ...state,
+                     dropdown: DataDropDown
+                }
+            } catch (e) {
+                return {
+                    ...state
+                }
+            }
+        default: return state
+    } 
+}
+
+export default todoReducer
